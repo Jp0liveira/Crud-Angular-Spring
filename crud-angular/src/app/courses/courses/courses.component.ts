5bffb9664fc4e2fd214ffc881b9eb760cc1e2b01
Joao Paulo Oliveira <joaooliveirawebdeveloper@gmail.com>
2023-01-17 12:01:45 -0300

    Service
    criação do service, aula 08

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -8,14 +9,12 @@ import { Course } from '../model/course';
 })
 export class CoursesComponent implements OnInit{
 
-  courses: Course[] = [
-    { _id: '1', name:'Angular', category: 'Front-End'}
-  ];
+  courses: Course[] = [ ];
   displayedColumns = ['name', 'category'];
 
-  constructor( ){
+  constructor( private coursesService: CoursesService ){
     // this.courses = [ ];
-
+    this.courses = this.coursesService.list( );
    }
 
   ngOnInit(): void {

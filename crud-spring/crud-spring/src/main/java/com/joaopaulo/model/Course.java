85b97c0cf61e47dc364fb13222a6d2cb39c6ba7a
Jp0liveira <joaooliveirawebdeveloper@gmail.com>
2023-01-19 11:22:32 -0300

    Consumo de API
    Aula 16, conexão do front end com o back end

--- crud-spring/crud-spring/src/main/java/com/joaopaulo/model/Course.java
+++ crud-spring/crud-spring/src/main/java/com/joaopaulo/model/Course.java
@@ -1,5 +1,7 @@
 package com.joaopaulo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,6 +17,7 @@ public class Course {
 
   @Id // chave primaria
   @GeneratedValue( strategy = GenerationType.AUTO )
+  @JsonProperty( "_id") // transforma o id em _id para funcionar no front-end ( angular )
   private Long id;
 
   @Column( length = 200, nullable = false )

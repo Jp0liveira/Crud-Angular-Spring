6a4e488e683e12a3193b5834450d2e0b191cda38
Jp0liveira <joaooliveirawebdeveloper@gmail.com>
2023-01-23 14:15:13 -0300

    Refatoração Angular Material Table

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -16,7 +16,6 @@ import { CoursesService } from '../services/courses.service';
 export class CoursesComponent implements OnInit{
 
   courses$: Observable <Course[]>;
-  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,

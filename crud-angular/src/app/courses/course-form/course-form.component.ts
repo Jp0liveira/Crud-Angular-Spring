5f676ece7dae3cbeeb1c87c9aa2c0ca960459f63
Jp0liveira <joaooliveirawebdeveloper@gmail.com>
2023-01-23 11:38:27 -0300

    Angular Typed Forms
    Refatoração do fomulário

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../services/courses.service';
@@ -11,16 +11,19 @@ import { CoursesService } from '../services/courses.service';
   styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent implements OnInit{
-  form: FormGroup;
 
-  constructor( private formBuilder: FormBuilder,
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
+
+  constructor( private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
-    private location: Location ){ // Location do angular
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
+    private location: Location){ // Location do angular
+    // this.form = this.formBuilder.group({
+    //   name: [null],
+    //   category: [null]
 
   }
   onSubmit() {

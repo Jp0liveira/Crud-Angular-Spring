a2c01df2af8ce129567a0720e084942cfb35a49f
Jp0liveira <joaooliveirawebdeveloper@gmail.com>
2023-01-23 10:46:32 -0300

    Formulário: API Spring: Criar Curso (HTTP POST)
    Salvando os cursos método post

--- crud-spring/crud-spring/src/main/java/com/joaopaulo/controller/CoursesController.java
+++ crud-spring/crud-spring/src/main/java/com/joaopaulo/controller/CoursesController.java
@@ -2,8 +2,14 @@ package com.joaopaulo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaopaulo.model.Course;
@@ -23,6 +29,16 @@ public class CoursesController {
   public List<Course>  list() {
     return courseRepository.findAll( );
 
-  } 
+  }
+
+  @PostMapping
+  @ResponseStatus( code = HttpStatus.CREATED )
+  public  Course create( @RequestBody Course course ){
+    return courseRepository.save(course);
+
+    // return ResponseEntity.status( HttpStatus.CREATED )
+    //       .body(courseRepository.save(course));
+
+  }
 
 }
